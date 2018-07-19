495f9265c252eb95ff3a58e3c62093498dc4a2e3
Pavel Morshenyuk <pavel.morshenyuk@cloudreach.com>
2018-07-19 12:17:23 +0200

    Upgraded release

--- Formula/sceptrefun.rb
+++ Formula/sceptrefun.rb
@@ -1,8 +1,8 @@
 class Sceptrefun < Formula
     desc "Sceptre Fun with sounds"
     homepage "https://github.com/esn89/homebrew-sceptrefun"
-    url "https://github.com/esn89/homebrew-sceptrefun/archive/v0.0.2.tar.gz"
-    sha256 "3cc57312c5b57002c9be5efa88982efda408641ccf3eefc85721715175246e82"
+    url "https://github.com/esn89/homebrew-sceptrefun/archive/v0.0.3.tar.gz"
+    sha256 "ba4f4ee019a391951780621e0b0d855e9cc77f3189b51358cc02e97dec002144"
   
     bottle do
         cellar :any_skip_relocation
