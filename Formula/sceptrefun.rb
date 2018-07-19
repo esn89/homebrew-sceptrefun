29e9dd86fc5467bc37dca83635f19e7087033d12
Pavel Morshenyuk <pavel.morshenyuk@cloudreach.com>
2018-07-19 10:22:46 +0200

    Added checksum for Formula

--- Formula/sceptrefun.rb
+++ Formula/sceptrefun.rb
@@ -4,6 +4,7 @@ class Sceptrefun < Formula
     desc "Sceptre Fun with sounds"
     homepage "https://github.com/esn89/homebrew-sceptrefun"
     url "https://github.com/esn89/homebrew-sceptrefun/archive/v0.0.1.tar.gz"
+    sha256 "4e0a89e07d9d6b1a872dd7bd13ba1196acc7eb3b744091a020d316a8bd56bd15"
   
     bottle do
         cellar :any_skip_relocation
